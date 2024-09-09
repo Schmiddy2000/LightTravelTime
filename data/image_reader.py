92c1ce2bef794e2541a5856334c748e4a14d8251
Schmiddy2000 <luca.schmitt@gmx.net>
2024-09-09 21:22:28 +0200

    Added amplitude reading capability to the image reader

--- data/image_reader.py
+++ data/image_reader.py
@@ -1,11 +1,47 @@
 # Imports
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-# Load the image
-image_index = 1
-img = mpimg.imread(f'Oscilloscope data/DS1Z_QuickPrint{image_index}.bmp')
 
-# Display the image
-plt.imshow(img)
-plt.show()
+def get_amplitude(image_index: int):
+    img = mpimg.imread(f'Oscilloscope data/DS1Z_QuickPrint{image_index}.bmp')
+
+    orange = [248, 128, 0, 255]
+    black = [0, 0, 0, 255]
+    gray = [48, 48, 48, 255]
+
+    disregarded_colors = [orange, black, gray]
+
+    left_start = 84
+    top_start = 39
+    right_stop = 683
+    bottom_stop = 396
+
+    start_row = 0
+    stop_row = 0
+    did_set_start_row = False
+    no_disregard_this_row = True
+
+    for i in range(top_start, bottom_stop):
+        no_disregard_this_row = True
+
+        for j in range(left_start, right_stop):
+            if list(img[i][j]) not in disregarded_colors:
+                no_disregard_this_row = False
+
+                if not did_set_start_row:
+                    start_row = i
+                    did_set_start_row = True
+
+            if j == right_stop - 1 and no_disregard_this_row and did_set_start_row:
+                stop_row = i
+
+                print(f'Start row: {start_row}, Stop row: {stop_row} --> Amplitude: {stop_row - start_row}')
+
+                return stop_row - start_row
+
+    # Display the image
+    # plt.imshow(img)
+    # plt.show()
+
+
+get_amplitude(1)
